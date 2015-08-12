3ab3e26abe33da26064e0b0382d753f617997d60
calebgregory <calebgregory@gmail.com>
2015-08-12 16:31:26 -0500

    Adding sample ascii-art and mouse-down, mouse-drag, and mouse-up functionality

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var clicking = false
+  , fontSize = 13
+  , currY;
+
+$('.container').mousedown(function(){
+  clicking = true;
+  $('.clickstatus').text('mousedown');
+
+  $(this).mousemove(function(e){
+    if(clicking == false) return;
+
+    // Mouse click + moving logic here
+    $('.movestatus').text('mouse moving');
+
+
+    if(e.pageY <= currY - 5 && fontSize < 28) {
+      if($(window).width() < $('pre').width()) return;
+      console.log('up');
+      fontSize++;
+    } else if(e.pageY > currY + 5 && fontSize > 4) {
+      console.log('down');
+      fontSize--;
+    }
+    console.log(currY,e.pageY,fontSize)
+    currY = e.pageY;
+
+    $('pre').css('font-size',fontSize+'px');
+  });
+
+});
+
+$(document).mouseup(function(){
+  clicking = false;
+  $('.clickstatus').text('mouseup');
+  $('.movestatus').text('click released, no more move event');
+})
+
