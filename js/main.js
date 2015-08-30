70912e4fb013a590f0bd21b15231224b022f3b3b
calebgregory <calebgregory@gmail.com>
2015-08-29 19:44:32 -0500

    Adding container flexibility - ability to get rid of container

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ $('.container').mousedown(function(){
     } else if(e.pageY > currY + 5 && fontSize > 4) {
       fontSize--;
     }
+    console.log(currY,e.pageY,fontSize)
     currY = e.pageY;
 
     $('pre').css('font-size',fontSize+'px');
@@ -32,3 +33,10 @@ $(document).mouseup(function(){
   $('.movestatus').text('click released, no more move event');
 })
 
+$('#no-container').on('click', function(e) {
+  $('.container').css('max-height', 1000);
+  $('.container').css('max-width', 1500);
+  $('.container pre').css('max-height', 1000);
+  $('.container pre').css('max-width', 1500);
+  e.preventDefault();
+})
